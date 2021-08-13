29f8978cedffb4d57c7a49a52cdf8a6ffcddf7c3
zetunejacques <jacques.zetune@inetsystem.com.br>
2021-08-13 12:19:47 -0300

    Classes de domínio

---
+++ Models/Historico/ExameCadastrado.cs
@@ -0,0 +1,15 @@
+namespace Symphony.Models
+{
+    public class ExameCadastrado
+    {
+        public string Origem { get; set; }
+        public string CodExmLab { get; set; }
+        public string DescExmLab { get; set; }
+        public string DescMatLab { get; set; }
+        public string CodExm { get; set; }
+        public string DescExm { get; set; }
+        public string CodMat { get; set; }
+        public string DescMat { get; set; }        
+    }
+
+}
--- Models/Historico/GetExamesSuspensos.cs
+++ Models/Historico/GetExamesSuspensos.cs
@@ -1,4 +1,11 @@
+using System.Collections.Generic;
+
 namespace Symphony.Models
 {
+    public class GetExamesSuspensos
+    {
+        public int CodigoLC { get; set; }
+        public List<RetornoExameSuspenso> Suspensos { get; set; }
+    }
 
 }
